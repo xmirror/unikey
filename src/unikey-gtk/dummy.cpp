50463c7450e0460f1cf1c096c6a1061159fa7382
Long K. Pham <unikey@gmail.com>
2005-10-08 12:15:41 +0000

    initial
    git-svn-id: file:///home/unikey/trunk@31 e1ceb1ec-800a-4d3f-98f3-0cab0dcde3af

---
+++ src/unikey-gtk/dummy.cpp
@@ -0,0 +1,6 @@
+//this file is used merely to force libtool to use g++ in linking
+
+int unikey_gtk_dummy()
+{
+  return 1;
+}
---
+++ src/xim/dummy.cpp
@@ -0,0 +1,6 @@
+//this file is used merely to force libtool to use g++ in linking with libUnikey
+
+int unikey_xim_dummy()
+{
+  return 1;
+}
