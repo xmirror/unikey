dea856531a06f5bb14d68db6ef779d95d0ce6ebf
Long K. Pham <unikey@gmail.com>
2005-10-15 22:18:34 +0000

    no message
    git-svn-id: file:///home/unikey/trunk@43 e1ceb1ec-800a-4d3f-98f3-0cab0dcde3af

--- src/ukengine/ukengine.h
+++ src/ukengine/ukengine.h
@@ -20,7 +20,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-
+//this engine is shared among x-unikey and win-unikey
 #ifndef __UKENGINE_H
 #define __UKENGINE_H
 
